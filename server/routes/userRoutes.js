b24cfc07e610f0790dfa49b565c254d94fb446ce
Sanjeev-Chakaravarthy <sanjeev.ms28122@gmail.com>
2025-04-03 11:14:41 +0530

    Implemented GET API Endpoints

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/userController');
+const { registerUser, loginUser, getUserProfile, getUserById, getConnections, getPotentialPartners,updateUserProfile } = require('../controllers/userController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
-router.post('/register', registerUser);
+router.post('/signup', registerUser);
 
 router.post('/login', loginUser);
 
+router.get('/profile', authMiddleware,getUserProfile);
+router.get('/:id', authMiddleware, getUserById);
+router.get('/connections/list', authMiddleware,getConnections);
+router.get('/partners/suggestions', authMiddleware, getPotentialPartners);
+router.put('/profile/update', authMiddleware, updateUserProfile);
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -25,6 +25,12 @@ app.use("/api/exchanges", exchangeRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/ratings', ratingRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    msg: 'Endpoint not found'
+  });
+});
 
 
 app.use(errorHandler);
