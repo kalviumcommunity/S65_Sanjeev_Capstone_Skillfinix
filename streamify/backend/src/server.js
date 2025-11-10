a20f364c528e62758185d65b1c55ad471c3332ea
Sanjeev MS <sanjeevms@Sanjeevs-MacBook.local>
2025-11-10 15:46:35 +0530

    add auth middleware for route protection and integrate Stream Chat user management and token generation

--- streamify/backend/src/server.js
+++ streamify/backend/src/server.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
+const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT;
 const authRoutes = require('./routes/authroute');
 const connectdb = require('./lib/db');
+
+
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth",authRoutes)
 
 app.listen(PORT,()=>{
